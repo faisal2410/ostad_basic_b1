53e7f219b1a92aef2670744458e2b9266e3b1970
Faisal ahmed <faisal2410@yahoo.com>
2023-05-15 22:34:18 +0600

    updated

---
+++ live_class/main.js
@@ -0,0 +1,21 @@
+// console.log("Bangladesh is my motherland");
+// console.log("We are learning javascript");
+
+let test;
+
+let employeeName = "Sajal Kanti Sinha";
+let employeeAddress = "Sylhet";
+let employeeSalary = 25550.50;
+let isActive = true;
+let employeeInfo = `We have 2 employee. The main person of our institute is ${employeeName} 
+
+his salary is ${employeeSalary}
+
+`
+// console.log(employeeInfo);
+
+console.log("==========>", typeof employeeName);
+console.log("🏆👍====>", typeof employeeSalary);
+console.log("🏆👍====>", typeof isActive);
+console.log("xxxxxxxxxxx", typeof test);
+
